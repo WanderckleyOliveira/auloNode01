89205c8dd200fd6f8abdfc5d0d3558e16fb8f1c3
Wanderckley Oliveira <Keiu_19@hotmail.com>
2023-11-21 21:23:59 -0400

    Criando uma API

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 app.get('/', (req, res) => {
-    res.send("Olá mundo!")
+    res.json({
+        nome: "Wanderckley",
+        sobreNome: "Oliveira"
+    })
+
+})
+
+app.get('/ws/69087590/json', (req, res) => {
+    res.json({
+        CEP: "69087590",
+        logradouro: "Rua Pirita",
+        completo: "lado impar",
+        bairro: "Tancredo Neves",
+        localidade: "Manaus",
+        Uf: "Amazonas",
+        ibge: "35849165164",
+        gia: "1004",
+        ddd: "92",
+        siafi: "7107"
+    })
+
 })
 
 app.get('/imagens', (req, res) => {
-    res.send("Olá imagens!")
+    res.send("Olá <strong>imagens!</strong>")
+})
+
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.resolve('sobre.html'))
+})
+
+app.get('/perguntas', (req, res) => {
+    res.sendFile(path.resolve('perguntas.html'))
 })
 
 app.listen(3000, () => {
