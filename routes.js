5af58afc32e7155833738f9e6652f04f47195ae2
Wanderckley Oliveira <Keiu_19@hotmail.com>
2023-11-29 22:42:15 -0400

    Deletando arquivo do projeto

--- routes.js
+++ routes.js
@@ -13,7 +13,6 @@ router.get('/livros', LivroController.index)
 router.get('/livros/:id', LivroController.show)
 router.post("/livros", LivroController.store)
 router.put('/livros/:id', LivroController.update)
-router.delete('/livros/:id', LivroController.delete)
 
 
 
